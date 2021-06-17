5924fd095fc7f6b34617ee38ffac78f6e0dce8b9
baejunong <51183527+baejunong@users.noreply.github.com>
2021-06-17 11:28:47 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-jo.herokuapp.com"
+    : "http://localhost:8080";
